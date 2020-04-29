b873deed28283e487ec441f62123bab6038d3faf
ligongxin <562500847@qq.com>
2020-04-30 00:01:55 +0800

    add logging

--- lgx_atm/conf/settings.py
+++ lgx_atm/conf/settings.py
@@ -1,2 +1,11 @@
 # __author__:'lgx'
 # date:2020/4/29 0029
+import os,sys
+#主路径
+BASE_DIR=os.path.dirname(os.path.dirname(__file__))
+
+#日志路径
+log_path=os.path.join(BASE_DIR,'log')
+
+
+print(BASE_DIR)
--- lgx_atm/core/logger.py
+++ lgx_atm/core/logger.py
@@ -1,2 +1,18 @@
 # __author__:'lgx'
 # date:2020/4/29 0029
+import logging
+from lgx_atm.conf import settings
+
+def logger():
+    logger=logging.getLogger('atm') #日志名称
+    logger.setLevel('DEBUG') #设置输出级别
+
+    #创建屏幕输出
+    ch=logging.StreamHandler()
+    ch.setLevel('DEBUG') #设置输出级别
+
+    #创建文件输出
+    #日志路径
+    log_file='%s'%(settings.log_path)
+    fh=logging.FileHandler(log_file)
+    fh.setLevel('DEBUG')
